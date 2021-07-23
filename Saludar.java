46dfc2658aaae3be5b7a9ea0d7d75a512b3e39ea
Carlos Villasante <villasante.carlos@outlook.com>
2021-07-23 10:12:32 +0200

    Update Saludar.java

--- Saludar.java
+++ Saludar.java
@@ -3,6 +3,8 @@ public class Saludar
 {
     public static void main(String args[])
      {
-        System.out.println("Este es mi primer ejemplo en Java");
+        for(String s:args){
+            System.out.println(s.toUpperCase());
+        }
     }
 }
